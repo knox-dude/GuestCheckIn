b09e17cc24263a1a0f0744cfad356769e8d7e056
Andrew Knox <andrew.knox.r@gmail.com>
2024-09-19 16:44:14 -0700

    Initial Commit

---
+++ GuestCheckIn/GuestCheckInApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuestCheckInApp.swift
+//  GuestCheckIn
+//
+//  Created by Andrew Knox on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct GuestCheckInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuestCheckInTests/GuestCheckInTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuestCheckInTests.swift
+//  GuestCheckInTests
+//
+//  Created by Andrew Knox on 9/19/24.
+//
+
+import Testing
+@testable import GuestCheckIn
+
+struct GuestCheckInTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
